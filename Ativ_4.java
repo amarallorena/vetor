c1f8221ec03915b073546fb55408215e5ea9e70e
amarallorena <159277845+amarallorena@users.noreply.github.com>
2024-05-29 11:13:25 -0300

    vetor

---
+++ Ativ_4.java
@@ -0,0 +1,23 @@
+package vetor;
+
+import java.util.ArrayList;
+
+public class Ativ_4 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> cinco = new ArrayList<>();
+		int soma=0;
+
+		cinco.add(20);
+		cinco.add(5);
+		cinco.add(10);
+		cinco.add(5);
+		cinco.add(10);
+
+		for(int i : cinco) {
+			soma+= i;
+		}
+		System.out.println("O resultado da sima é igual a: " +soma );
+	}
+
+}
